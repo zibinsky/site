f792ce7534230fa7beb8fa111db5da69cd32589e
Yuri <47826736+zibinsky@users.noreply.github.com>
2020-06-12 23:05:27 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,108 @@
+gsap.from(".left-col", 2, {
+   width: "0%",
+   ease: Expo.easeInOut
+})
+
+gsap.from(".logo", 2, {
+   delay: 2.5,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".buy", 2, {
+   delay: 2.8,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".year", 2, {
+   delay: 3.1,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+gsap.from(".album-name", 2, {
+   delay: 2.4,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".album-artist", 2, {
+   delay: 2.6,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".about h1", 2, {
+   delay: 3.6,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".album-name2", 2, {
+   delay: 3.8,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".album-artist2", 2, {
+   delay: 4,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".album-genre2", 2, {
+   delay: 4.2,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".media p", 2, {
+   delay: 4.4,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+TweenMax.staggerFrom(".media ul li", 2, {
+   delay: 4.8,
+   opacity: 0,
+   y: 20,
+   ease: Expo.easeInOut
+}, 0.2);
+
+gsap.from(".album-name3", 2, {
+   delay: 5.6,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".album-artist3", 2, {
+   delay: 5.8,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+gsap.from(".next", 2, {
+   delay: 6,
+   y: 20,
+   opacity: 0,
+   ease: Expo.easeInOut
+})
+
+
+
+
+
+
+
